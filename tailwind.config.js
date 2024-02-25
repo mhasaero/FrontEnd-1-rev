91e0825106dedeb824d2064667f930dceae780b7
mhasaero <147121238+mhasaero@users.noreply.github.com>
2024-02-25 23:33:35 +0700

    feat(section): add hero section
    ngepush hero section with responsive tapi masih belum yakin 100%, nambahin breakpoint baru buat hp fold kendala di pembuatan 3 breakpoint responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      fold: "200px",
+      md: "768px",
+      lg: "1025px",
+    },
   },
   plugins: [],
 };
