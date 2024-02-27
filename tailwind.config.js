95196762bf8b55923358fed90fe8aa46892e25ce
mhasaero <147121238+mhasaero@users.noreply.github.com>
2024-02-27 09:58:35 +0700

    change(style): apply Helvetica Neue Light
    mengaplikasikan font helvetica neue light pada seluruh body

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
-    colors: {
-      "navbar-color": "rgba(217, 217, 217)",
+    fontFamily: {
+      base: ["HelveticaNeue"],
     },
     boxShadow: {
       card: "0px 0px 20px 0px rgb(0 0 0 / 0.1)",
